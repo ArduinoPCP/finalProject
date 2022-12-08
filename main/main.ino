b645a80f23d99f12cd95f2356b70d5530770d186
Alice.Frezza <alice.frezza@stud-inf.unibz.it>
2022-12-08 17:07:57 +0100

    added comments

--- main/main.ino
+++ main/main.ino
@@ -10,7 +10,7 @@ void setup() {
   Serial.begin(9600);
   randomSeed(42);
   pinMode(buttonPin, INPUT); 
-  
+
 }
 
 void loop() {
@@ -20,6 +20,9 @@ void loop() {
   }
 }
 
+//this function terminates and returns true when the user presses the button successfully
+//since the robot is unconfortable, the user might have to press the button multiples 
+//times before it actually works
 bool robotIsWorking()
 {
     bool isWorking = false;
@@ -29,7 +32,7 @@ bool robotIsWorking()
         buttonState = digitalRead(buttonPin);
         if (buttonState == HIGH)
         {
-            if (long(buttonState) == generateRandomValue())
+            if (long(buttonState) == generateRandomValue()) 
             {
                 Serial.print("robot working here");
                 isWorking = true;
@@ -41,16 +44,19 @@ bool robotIsWorking()
     return true;
 }
 
+//this function generates a random value from 0 to 2
 long generateRandomValue()
 {
     return random(1,4)-1;
 }
 
+//this function sets up the motor
 void setUpMotor()
 {
     myservo.attach(9);  // Attach the servo to pin 9
 }
 
+//this function makes the motor move
 void powerMotor()
 {
     myservo.write(45); // The speed at which the motor rotates
